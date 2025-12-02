fb815ab475eddb52bbc9057732b9c9e504f7164b
Sreesairam V <sairam2381@users.noreply.github.com>
2025-12-02 15:06:34 +0530

    The Flex is pushed

--- src/hello-world/hello-world-component.jsx
+++ src/hello-world/hello-world-component.jsx
@@ -3,7 +3,13 @@ import { HelloWorldStyle } from "./hello-world-style";
 const HelloWorldComponent = () => {
   return (
     <HelloWorldStyle>
-      <h1 className="element">Sri eshwar</h1>
+      {/* <h1 className="element">Sri eshwar</h1> */}
+      <div className="parent">
+        <div className="square__child"></div>
+        <div className="square__child"></div>
+        <div className="square__child"></div>
+        <div className="square__child"></div>
+      </div>
     </HelloWorldStyle>
   );
 };
--- src/hello-world/hello-world-style.js
+++ src/hello-world/hello-world-style.js
@@ -1,7 +1,18 @@
 import styled from "styled-components";
 export const HelloWorldStyle = styled.div`
-  background-color: green;
-  .element {
-    color: red;
+  .parent {
+    display: flex;
+    background-color: green;
+    //align-items: flex-start;
+    // align-items: center;
+    align-items: flex-start;
+    justify-content: flex-end;
+    gap: 20px;
+    height: 100vh;
+  }
+  .square__child {
+    width: 50px;
+    height: 50px;
+    background-color: yellow;
   }
 `;
